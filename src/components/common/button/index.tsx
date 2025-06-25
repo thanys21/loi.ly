f64e230af3ca9ce8a99b04d17c803d5328670f84
ThaNys <loithienly@gmail.com>
2025-06-25 21:09:52 +0700

    feat: import-library-shadcn-ui

--- src/components/common/button/index.tsx
+++
@@ -1,7 +0,0 @@
-import { type JSX } from "react";
-
-const Button = (): JSX.Element => {
-  return <div>Button</div>;
-};
-
-export default Button;
---
+++ src/components/landing-page/index.tsx
@@ -0,0 +1,16 @@
+import { Button } from "../ui/button";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div>
+        <Button />
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;

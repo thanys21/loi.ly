92d0296702e684b8144d0a66186c9baec8fa74c8
ThaNys <loithienly@gmail.com>
2025-06-30 20:27:17 +0700

    feat: add-deploy-project

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/loi.ly/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
